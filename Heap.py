c74d6cc05c0aaf38acd3e3ade0822333debd9114
RajaRohan Reddy Vaidyula <139357615+rajarohan@users.noreply.github.com>
2025-08-06 10:01:33 +0530

    Create Heap.py

---
+++ Heap.py
@@ -0,0 +1,23 @@
+def find_parent(i, a):
+    if i == 0:
+        return 
+    print(a[(i - 1) // 2])
+def find_left_child(i, a):
+    idx = 2 * i + 1
+    if idx >= len(a):
+        return 
+    print(a[idx])
+def find_right_child(i, a):
+    idx = 2 * i + 2
+    if idx >= len(a):
+        return 
+    print(a[idx])
+
+import heapq
+
+a = [10, 20, 15, 30, 40, 5, 8]
+
+heapq.heapify(a)
+
+print("Heap array:", a)
+find_right_child(2, a)
