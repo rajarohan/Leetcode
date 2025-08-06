b29a180f3938eb6468ec90c3823268b62808e286
RajaRohan Reddy Vaidyula <139357615+rajarohan@users.noreply.github.com>
2025-08-06 12:41:57 +0530

    Create Length_of_longest_strictly_increasing_subsequence.py

---
+++ Length_of_longest_strictly_increasing_subsequence.py
@@ -0,0 +1,26 @@
+def allsubseq(a):
+    n = len(a)
+    ans = []
+    def bt(i, ds):
+        if i == n:
+            ans.append(ds[:])
+        ds.append(a[i])
+        bt(i + 1, ds)
+        ds.pop()
+        bt(i + 1, ds)
+    bt(0, [])
+    return ans
+def is_increasing(a):
+    for i in range(1, len(a)):
+        if a[i] < a[i - 1]:
+            return False
+    return True
+
+def longest_Subsequence(a):
+    all = allsubseq(a)
+    maxi = 0
+    for i in all:
+        if is_increasing(i):
+            maxi = max(maxi, len(i))
+    return maxi
+      
