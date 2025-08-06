05f730c999226f365f716776b10c76cb5fc30cd9
RajaRohan Reddy Vaidyula <139357615+rajarohan@users.noreply.github.com>
2025-08-06 12:14:12 +0530

    added Memoization

--- Climbing_Stairs.py
+++ Climbing_Stairs.py
@@ -1,3 +1,4 @@
+#Tabulation(bottom-up)
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
@@ -10,3 +11,19 @@ class Solution:
         for i in range(3, n + 1):
             dp[i] = dp[i - 1] + dp[i - 2]
         return dp[n]
+
+#Memoization(Top-Down)
+class Solution:
+    def __init__(self):
+        self.memo = {}
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        if n in self.memo:
+            return self.memo[n]
+
+        self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.memo[n]
